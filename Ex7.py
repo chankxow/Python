84965dda8742ab38102789f9faa860b014f41454
chankxow <101338054+chankxow@users.noreply.github.com>
2022-07-01 22:33:16 +0700

    update
    up

---
+++ Ex7.py
@@ -0,0 +1,12 @@
+
+width = float(input("ความสูง :"))
+length = float(input("ความยาว :"))
+
+if width and length > 0:
+    print("พื้นที่สามเหลี่aยมนี้มีขนาด คือ ",float(width*length*0.5))
+elif width and length <= 0 :
+    print("กรุณากรอกตัวเลขจำนวนเต็มบวก")
+else :
+    print("กรุณากรอกตัวเลขจำนวนเต็มบวก")
+    
+
---
+++ Ex8.py
@@ -0,0 +1,6 @@
+kristsak = int(input("ปี : "))
+puttaasak = kristsak + 543
+if kristsak >= 0 :
+    print(f"ปีพุทธศักราช นี้คือ :{puttaasak} ")
+else :
+    print("“กรุณากรอกข้อมูลที่มากกว่าหรือเท่ากับ 0”")
